07d25df6f40b40d3555552e3788c4ef76ede1f5a
Nam Titan <namtitan@Nam-Titans.local>
2015-07-22 15:29:00 +0700

    Initial Commit

---
+++ AllOfUIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllOfUIView
+//
+//  Created by Nam Titan on 7/22/15.
+//  Copyright © 2015 Nam Titan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AllOfUIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllOfUIView
+//
+//  Created by Nam Titan on 7/22/15.
+//  Copyright © 2015 Nam Titan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
